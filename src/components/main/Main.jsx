60fbf1dae60eb00da227620f460adcdfc83fe3e9
timondigital <timondigitalweb@hotmail.com>
2022-08-23 10:00:17 -0300

    add btn whatsapp

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,11 +4,15 @@ import Home from "./home/Home";
 
 //styles
 import styles from "./main.module.scss";
+import Popup from "../popup/Popup";
+import BtnWhatsapp from "../buttons/whatsapp/BtnWhatsapp";
 
 const Main = () => {
   return (
     <main className={styles.main}>
       <Alert />
+      <Popup />
+      <BtnWhatsapp />
       <Home />
     </main>
   );
